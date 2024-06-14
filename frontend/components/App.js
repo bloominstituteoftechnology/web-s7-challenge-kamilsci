5e222b80383a6f4882d9d796303c26e6fec6446c
kamilsci <sunnysideprd@gmail.com>
2024-06-14 14:58:39 -0700

    HALFWAY

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import Home from './Home'
 import Form from './Form'
+import { NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div id="app">
       <nav>
-        {/* NavLinks here */}
+        <NavLink to="/" exact activeClassName="active">Home</NavLink>
+        <NavLink to="/order" activeClassName="active">Order</NavLink>
       </nav>
       {/* Route and Routes here */}
-      <Home />
-      <Form />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/order" element={<Form />} />
+
+      </Routes>
+
     </div>
   )
 }
--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import pizza from './images/pizza.jpg'
+import { Link } from 'react-router-dom'
 
 function Home() {
+
+  function imgComp(){
+
+
+  }
+
   return (
+
+    
     <div>
       <h2>
         Welcome to Bloom Pizza!
       </h2>
       {/* clicking on the img should navigate to "/order" */}
+    
+      <Link to="/order">
       <img alt="order-pizza" style={{ cursor: 'pointer' }} src={pizza} />
+      </Link>
     </div>
   )
 }
